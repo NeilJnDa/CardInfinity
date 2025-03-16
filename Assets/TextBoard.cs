aae7394f5998b0a38bd636c21f608ae9fdfb8fe8
Jinda <neil_jnda@outlook.com>
2025-03-16 12:16:05 +0200

    UI

---
+++ Assets/TextBoard.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextBoard : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    private TMP_Text text;
+
+    
+}
